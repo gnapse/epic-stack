b1139bc5a78bd2b15b5453ed14fb34dfed860c91
Ernesto García <ernesto@ernesto.dev>
2024-03-13 20:17:22 -0300

    Switch to Ariakit's Checkbox, remove use of Radix UI's Label

--- app/components/forms.tsx
+++ app/components/forms.tsx
@@ -88,30 +88,28 @@ export function TextareaField({
 }
 
 export function CheckboxField({
-	labelProps,
-	buttonProps,
+	id,
+	label,
 	errors,
 	className,
+	...props
 }: {
-	labelProps: JSX.IntrinsicElements['label']
-	buttonProps: CheckboxProps & {
-		name: string
-		form: string
-		value?: string
-	}
+	id?: string
+	label: string
 	errors?: ListOfErrors
-	className?: string
-}) {
-	const { key, defaultChecked, ...checkboxProps } = buttonProps
+	name: string
+	form: string
+	value?: string
+} & Omit<CheckboxProps, 'name' | 'form' | 'value'>) {
+	const { key, defaultChecked, ...checkboxProps } = props
 	const fallbackId = useId()
-	const checkedValue = buttonProps.value ?? 'on'
+	const checkedValue = props.value ?? 'on'
 	const input = useInputControl({
 		key,
-		name: buttonProps.name,
-		formId: buttonProps.form,
+		name: props.name,
+		formId: props.form,
 		initialValue: defaultChecked ? checkedValue : undefined,
 	})
-	const id = buttonProps.id ?? fallbackId
 	const errorId = errors?.length ? `${id}-error` : undefined
 
 	return (
@@ -119,28 +117,23 @@ export function CheckboxField({
 			<div className="flex gap-2">
 				<Checkbox
 					{...checkboxProps}
-					id={id}
+					label={label}
+					id={id ?? fallbackId}
 					aria-invalid={errorId ? true : undefined}
 					aria-describedby={errorId}
 					checked={input.value === checkedValue}
-					onCheckedChange={state => {
-						input.change(state.valueOf() ? checkedValue : '')
-						buttonProps.onCheckedChange?.(state)
+					onChange={event => {
+						input.change(event.target.checked ? checkedValue : '')
+						props.onChange?.(event)
 					}}
 					onFocus={event => {
 						input.focus()
-						buttonProps.onFocus?.(event)
+						props.onFocus?.(event)
 					}}
 					onBlur={event => {
 						input.blur()
-						buttonProps.onBlur?.(event)
+						props.onBlur?.(event)
 					}}
-					type="button"
-				/>
-				<label
-					htmlFor={id}
-					{...labelProps}
-					className="self-center text-body-xs text-muted-foreground"
 				/>
 			</div>
 			<div className="px-4 pb-3 pt-1">
