3aa6ea069ae8da8e7506ecedf08ecc7edd635939
Ernesto García <ernesto@ernesto.dev>
2024-04-25 07:19:13 -0400

    Small code changes

--- app/components/spacer.tsx
+++ app/components/spacer.tsx
@@ -1,6 +1,17 @@
-export function Spacer({
-	size,
-}: {
+type Size =
+	| '4xs'
+	| '3xs'
+	| '2xs'
+	| 'xs'
+	| 'sm'
+	| 'md'
+	| 'lg'
+	| 'xl'
+	| '2xl'
+	| '3xl'
+	| '4xl'
+
+type Props = {
 	/**
 	 * The size of the space
 	 *
@@ -26,32 +37,23 @@ export function Spacer({
 	 *
 	 * 4xl: h-44 (176px)
 	 */
-	size:
-		| '4xs'
-		| '3xs'
-		| '2xs'
-		| 'xs'
-		| 'sm'
-		| 'md'
-		| 'lg'
-		| 'xl'
-		| '2xl'
-		| '3xl'
-		| '4xl'
-}) {
-	const options: Record<typeof size, string> = {
-		'4xs': 'h-4',
-		'3xs': 'h-8',
-		'2xs': 'h-12',
-		xs: 'h-16',
-		sm: 'h-20',
-		md: 'h-24',
-		lg: 'h-28',
-		xl: 'h-32',
-		'2xl': 'h-36',
-		'3xl': 'h-40',
-		'4xl': 'h-44',
-	}
-	const className = options[size]
-	return <div className={className} />
+	size: Size
+}
+
+const options: Record<Size, string> = {
+	'4xs': 'h-4',
+	'3xs': 'h-8',
+	'2xs': 'h-12',
+	xs: 'h-16',
+	sm: 'h-20',
+	md: 'h-24',
+	lg: 'h-28',
+	xl: 'h-32',
+	'2xl': 'h-36',
+	'3xl': 'h-40',
+	'4xl': 'h-44',
+}
+
+export function Spacer({ size }: Props) {
+	return <div className={options[size]} />
 }
